81b51e21ad49405cf9fb91d9ae90e28fcf194f70
Elbdan1 <99385539+Elbdan1@users.noreply.github.com>
2026-01-19 21:01:02 +0300

    Update: dim_channels dimension with channel-level metrics

--- medical_warehouse/models/marts/dim_channels.sql
+++ medical_warehouse/models/marts/dim_channels.sql
@@ -1,18 +1,18 @@
 -- models/marts/dim_channels.sql
 with channel_data as (
     select
-        sender as channel_name,
+        channel_name,
         case
-            when sender ilike '%pharma%' then 'Pharmaceutical'
-            when sender ilike '%cosmetic%' then 'Cosmetics'
+            when channel_name ilike '%pharma%' then 'Pharmaceutical'
+            when channel_name ilike '%cosmetic%' then 'Cosmetics'
             else 'Medical'
         end as channel_type,
         min(message_date) as first_post_date,
         max(message_date) as last_post_date,
         count(*) as total_posts,
-        avg(message_length) as avg_views
+        avg(view_count) as avg_views
     from {{ ref('stg_telegram_messages') }}
-    group by sender
+    group by channel_name
 )
 
 select
